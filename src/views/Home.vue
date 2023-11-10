f3d1daeb855e759a4fba6bdb3c95d6c2dada4733
nurawiguna <nuraaws11@gmail.com>
2023-11-10 19:27:55 +0700

    add description with comment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
         <ion-content>
             <ion-card>
             <ion-card-content>
+                <!-- dibawah ini adalah komponen ion-row dan ion-button yang akan kita gunakan untuk mengambil data dari API -->
                 <ion-row class="ion-justify-content-center ion-align-items-center">
                     <ion-button @click="fetchCryptocurrencyData" expand="medium">Get Data</ion-button>
                 </ion-row>
@@ -19,6 +20,8 @@
                     <ion-label>Symbol</ion-label>
                     <ion-label>Harga USD</ion-label>
                 </ion-item>
+                
+                <!-- dibawah ini adalah komponen ion-item yang akan kita gunakan untuk menampilkan data dari API -->
                 <ion-item v-for="crypto in cryptoData" :key="crypto.id">
                     <ion-label>{{ crypto.name }}</ion-label>
                     <ion-label>{{ crypto.symbol }}</ion-label>
@@ -31,23 +34,25 @@
     </ion-page>
 </template>
 
+
 <script>
     import { defineComponent } from 'vue';
-    import axios from 'axios';
+    import axios from 'axios'; 
     
     export default defineComponent({
-        name: 'Home',
+        name: 'Home', // nama komponen yang akan kita gunakan
         data() {
             return {
-                cryptoData: [],
+                cryptoData: [], // variabel untuk menampung data dari API
             };
         },
         
         methods: {
             async fetchCryptocurrencyData() {
                 try {
-                    const response = await axios.get('https://api.coinlore.net/api/tickers/');
-                    this.cryptoData = response.data.data;
+                    const response = await axios.get('https://api.coinlore.net/api/tickers/'); // API yang digunakan untuk mengambil data cryptocurrency
+                    this.cryptoData = response.data.data; // data yang diambil disimpan ke dalam variabel cryptoData
+                
                 } catch (error) {
                     console.error('Error fetching cryptocurrency data:', error);
                 }
